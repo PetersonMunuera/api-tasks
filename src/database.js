329b400767351388b2f7919d19e6df658bb48c7f
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-20 20:48:07 -0300

    feat: database persist

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     let data = this.#database[table] ?? []
@@ -9,10 +27,12 @@ export class Database {
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data) 
+      this.#database[table].push(data)
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
--- src/server.js
+++ src/server.js
@@ -3,10 +3,9 @@ import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
+  const { method, url } = req
 
   await json(req, res)
-
-  const { method, url } = req
 
   const route = routes.find(route => route.method === method && route.path === url)
 
