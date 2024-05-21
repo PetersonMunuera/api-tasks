bdc5cd9d4ef0e6b1328ab6961c4a08ff76ab8c73
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-21 10:58:00 -0300

    feat: select filter

--- src/database.js
+++ src/database.js
@@ -25,8 +25,16 @@ export class Database {
     return data
   }
 
-  select(table) {
+  select(table, search) {    
     let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,12 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      const tasks = database.select('tasks')
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search,
+      }: null)
 
       return res.end(JSON.stringify(tasks))
     }
